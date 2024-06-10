6d99657520544097ca846e606a71fc9c8e5c1b6b
Komal Khatal <komalkhatal24@gmail.com>
2024-06-10 21:54:59 +0530

    Addition commit

---
+++ Add.cpp
@@ -0,0 +1,11 @@
+//Addition of two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b ,sum;
+    cout<<"Enter a two no"<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
