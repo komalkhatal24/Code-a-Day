0e5420389a739a398714124a841270c66816a3c6
Komal Khatal <komalkhatal24@gmail.com>
2024-06-15 10:19:42 +0530

    Commit

---
+++ HackerConditional.cpp/HackerArray.cpp/Array.cpp/BinarySearch.cpp
@@ -0,0 +1,33 @@
+//Binary serch in array
+//time complexity of binary search in 0(log2n)
+#include<iostream>
+using namespace std;
+int binarySearch(int arr[],int n,int key){
+    int s=0; 
+    int e=n;
+    while(s<=e){
+        int mid=s+e/2;
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<binarySearch(arr,n,key);
+    return 0;
+}
