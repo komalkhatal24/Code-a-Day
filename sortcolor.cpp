28bf988cb4b949d79e89f7a13ad5b6371567de65
Komal Khatal <komalkhatal24@gmail.com>
2024-07-23 11:49:38 +0530

    Commit

--- sortcolor.cpp
+++ sortcolor.cpp
@@ -21,7 +21,14 @@ public:
             if(nums[j]==0){
                 swap(nums[i],nums[j]);
                 i++;
-…            }
+                j--;           }
+                 else if(nums[j]==1){
+                j++;
+            }
+            else{
+                swap(nums[j],nums[k]);
+                k--;
+            }
          }
-    
+    }
 };
