2e94857691be7d95e5217cd4f92fafc19df18d0e
Komal Khatal <komalkhatal24@gmail.com>
2024-07-27 09:46:35 +0530

    commit

---
+++ DuplicatELeetcode.cpp
@@ -0,0 +1,26 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // if(nums.size()<2){return false;}
+        sort(nums.begin(),nums.end());
+        int start=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[start]==nums[i])
+            // if(nums[i]==nums[i+1])
+            {
+                return true;
+            }
+            start++;
+           
+        }
+        return false;
+
+    }
+};
