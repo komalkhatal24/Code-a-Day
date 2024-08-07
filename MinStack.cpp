98243f7f73cc4345ed12277836510c1be7c912c6
Komal Khatal <komalkhatal24@gmail.com>
2024-08-07 23:42:08 +0530

    commit

--- MinStack.cpp
+++ MinStack.cpp
@@ -10,10 +10,7 @@ int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
 
- 
-
 Example 1:
-
 Input
 ["MinStack","push","push","push","getMin","pop","top","getMin"]
 [[],[-2],[0],[-3],[],[],[],[]]
